c824801bc4b6618f707ab94234f1a5a94bb864af
gabiadochris <goldenlampstandministry777@gmail.com>
2025-07-01 18:42:18 +0100

    added a vercel.json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdminDashboard from './pages/Dashboard/AdminDashBoard';
 import AddMediator from './pages/AddUsersFormPage/AddMediator';
 import GetAllMediators from './pages/FetchAllInfo/AllMediatorsInfo';
-import Navbar from './component/NavBar';
+import Navbar from './component/NavBar'; // if you have a navbar
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Route path="/" element={<AdminDashboard />} />
         <Route path="/add-mediators" element={<AddMediator />} />
         <Route path="/get-all-mediators" element={<GetAllMediators />} />
+        <Route path="*" element={<Navigate to="/" />} /> {/* Catch-all */}
       </Routes>
     </Router>
   );
